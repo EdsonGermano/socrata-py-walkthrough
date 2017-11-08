56901627fd3760987a8133a16710fbc5c5517aac
Chris Duranti <chrisd1891@gmail.com>
2017-11-08 10:13:52 -0800

    Update usdot-points.py

--- examples/usdot-points.py
+++ examples/usdot-points.py
@@ -18,7 +18,7 @@ with open('/home/chris/Downloads/1000.csv', 'rb') as file:
   (ok, output) = output\
       .change_column_metadata('coredata_position', 'field_name').to('coredata_position_lat')\
       .change_column_metadata('coredata_position_1', 'field_name').to('coredata_position_long')\
-      .add_column('coredata_point', 'CoreData Point', 'make_point(to_number(coredata_position), to_number(coredata_position_1))', 'this is a column squared')\
+      .add_column('coredata_point', 'CoreData Point', 'make_point(to_number(coredata_position), to_number(coredata_position_1))', 'this is a point column')\
       .run()
 
   assert ok, output
