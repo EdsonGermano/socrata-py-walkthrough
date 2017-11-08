7a9359b3ccfb2438f8359e9d0675467aa31c4fb4
Chris Duranti <chrisd1891@gmail.com>
2017-11-08 10:05:45 -0800

    example of NBE point dataset

---
+++ examples/usdot-points.py
@@ -0,0 +1,26 @@
+from socrata.authorization import Authorization
+from socrata import Socrata
+import os
+
+auth = Authorization(
+  "data.transportation.gov",
+  os.environ['SOCRATA_USERNAME'],
+  os.environ['SOCRATA_PASSWORD']
+)
+
+with open('/home/chris/Downloads/1000.csv', 'rb') as file:
+  (revision, output) = Socrata(auth).create(
+      name = "usdot",
+      description = "a description"
+  ).csv(file)
+
+
+  (ok, output) = output\
+      .change_column_metadata('coredata_position', 'field_name').to('coredata_position_lat')\
+      .change_column_metadata('coredata_position_1', 'field_name').to('coredata_position_long')\
+      .add_column('coredata_point', 'CoreData Point', 'make_point(to_number(coredata_position), to_number(coredata_position_1))', 'this is a column squared')\
+      .run()
+
+  assert ok, output
+
+  revision.open_in_browser()
